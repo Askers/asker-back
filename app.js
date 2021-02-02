f86cca2573d36dd93b448bf8e86cad6eb244e9a9
hyesech <hyesech@gmail.com>
2021-02-02 22:27:03 +0900

    + 회원가입 구현

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const askRouter = require("./routes/ask");
 const userRouter = require("./routes/ask");
 const db = require("./models");
+const { urlencoded } = require("express");
 
 const app = express();
 
@@ -13,13 +14,17 @@ db.sequelize
   })
   .catch(console.error);
 
+// Front 연결
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+
 // Router
 app.get("/", (req, res) => {
   res.send("hello home");
 });
 
-app.use("/asks", askRouter);
-app.use("/users", userRouter);
+app.use("/ask", askRouter);
+app.use("/user", userRouter);
 
 app.listen(3065, () => {
   console.log("Listening on PORT 3065...");
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,33 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const { User } = require("../models/user");
+
 const router = express.Router();
+
+// signup Router: POST /user
+router.post("/", async (req, res, next) => {
+  try {
+    // 기존 유저와 매치
+    const exUser = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    if (exUser) {
+      return res.status(403).send("이미 사용 중인 email 입니다.");
+    }
+    // password hash
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    await User.create({
+      email: req.body.email,
+      username: req.body.username,
+      password: hashedPassword,
+    });
+    res.status(201).send("ok");
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
 
 module.exports = router;
